f2c80314562daaede3ef36a0d993f24d2dd8d19a
Trevor Makes <the.trevor.makes@gmail.com>
2022-11-28 01:42:50 -0800

    Implement bus with latches

---
+++ README.md
@@ -0,0 +1,23 @@
+# ATmega328p EEPROM Programmer
+
+- TODO
+
+## Building the software
+
+Use the [PlatformIO](https://platformio.org/) plugin for [VSCode](https://code.visualstudio.com/).
+
+Open the project folder with VSCode, select the environment for your board (`uno`, `nano`, `oldnano`), and click `Upload`.
+
+![](images/platformio.png)
+
+The [core](https://github.com/trevor-makes/core) library is required and PlatformIO will download this into the `.pio` folder.
+
+Distributed under the [MIT license](LICENSE.txt)
+
+## Assembling the circuit
+
+TODO
+
+## Using the programmer
+
+TODO
---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+// Copyright (c) 2022 Trevor Makes
+
+#include "core/io.hpp"
+#include "core/io/bus.hpp"
+#include "core/cli.hpp"
+#include "core/mon/format.hpp"
+
+#include <Arduino.h>
+
+using core::cli::Args;
+core::serial::StreamEx serialEx(Serial);
+core::cli::CLI<> serialCli(serialEx);
+
+#ifdef __AVR_ATmega328P__
+CORE_PORT(B)
+CORE_PORT(C)
+CORE_PORT(D)
+
+using MSBLatch = PortC::Bit<5>;
+using LSBLatch = PortC::Bit<4>;
+using ReadEnable = PortC::Bit<3>;
+using WriteEnable = PortC::Bit<2>;
+
+// [D7 D6 D5 D4 B3 B2 B1 B0]
+using DataBus = core::io::BitExtend<PortD::Mask<0xF0>, PortB::Mask<0x0F>>;
+#else
+#error Need to provide configuration for current platform. See __AVR_ATmega328P__ configuration above.
+#endif
+
+// TODO move into core::io?
+template <typename DATA, typename LATCH_ENABLE/*, typename OUTPUT_ENABLE*/>
+struct Latch {
+  using TYPE = typename DATA::TYPE;
+  static inline void config_output() {
+    // TODO enable OUTPUT_ENABLE
+    // TODO should LATCH_ENABLE be configured here?
+    LATCH_ENABLE::config_output();
+    LATCH_ENABLE::clear();
+  }
+  static inline void config_input() {
+    // TODO disable OUTPUT_ENABLE
+  }
+  static inline void write(TYPE data) {
+    // TODO may need to push/pop DDR, especially when bus is doing a read
+    // TODO maybe config_* should be private and done as part of read/write
+    //DATA::config_output();
+    DATA::write(data);
+    LATCH_ENABLE::set();
+    LATCH_ENABLE::clear();
+  }
+};
+
+using AddressMSB = Latch<DataBus, MSBLatch>;
+using AddressLSB = Latch<DataBus, LSBLatch>;
+using AddressBus = core::io::WordExtend<AddressMSB, AddressLSB>;
+
+using Control = core::io::Control<ReadEnable, WriteEnable>;
+using Bus = core::io::Bus<AddressBus, DataBus, Control>;
+
+void setup() {
+  Bus::config_write();
+
+  // Establish serial connection with computer
+  Serial.begin(9600);
+  while (!Serial) {}
+}
+
+void write_lsb(Args);
+void write_msb(Args);
+void write_data(Args);
+void write_bus(Args);
+
+void loop() {
+  static const core::cli::Command commands[] = {
+    { "lsb", write_lsb },
+    { "msb", write_msb },
+    { "data", write_data },
+    { "bus", write_bus },
+  };
+
+  serialCli.run_once(commands);
+}
+
+void write_lsb(Args args) {
+  uint8_t data = 0;
+  core::mon::parse_unsigned(data, args.next());
+  AddressLSB::write(data);
+}
+
+void write_msb(Args args) {
+  uint8_t data = 0;
+  core::mon::parse_unsigned(data, args.next());
+  AddressMSB::write(data);
+}
+
+void write_data(Args args) {
+  uint8_t data = 0;
+  core::mon::parse_unsigned(data, args.next());
+  DataBus::write(data);
+}
+
+void write_bus(Args args) {
+  uint16_t addr = 0;
+  uint8_t data = 0;
+  core::mon::parse_unsigned(addr, args.next());
+  core::mon::parse_unsigned(data, args.next());
+  Bus::write_byte(addr, data);
+}
