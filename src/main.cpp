b1800fa633744cff61b53af7f9ef767bf7b5f1d2
Trevor Makes <the.trevor.makes@gmail.com>
2022-12-03 01:12:26 -0800

    Import types from core::io

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,15 @@
 #include <Arduino.h>
 
 using core::cli::Args;
+using core::mon::parse_unsigned;
 core::serial::StreamEx serialEx(Serial);
 core::cli::CLI<> serialCli(serialEx);
+
+using core::io::ActiveLow;
+using core::io::ActiveHigh;
+using core::io::BitExtend;
+using core::io::WordExtend;
+using core::io::Latch;
 
 // EEPROM needs at least 70 ns before reading data after pulling chip select low
 // Wrap the data port with this template to insert the required delay
@@ -33,18 +40,18 @@ CORE_PORT(C)
 CORE_PORT(D)
 
 // 8-bit data bus [D7 D6 D5 D4 B3 B2 B1 B0]
-using DataPort = core::io::BitExtend<PortD::Mask<0xF0>, PortB::Mask<0x0F>>;
+using DataPort = BitExtend<PortD::Mask<0xF0>, PortB::Mask<0x0F>>;
 
 // Latch upper and lower bytes of address from data port
-using MSBLatch = core::io::ActiveHigh<PortC::Bit<5>>;
-using LSBLatch = core::io::ActiveHigh<PortC::Bit<4>>;
-using AddressMSB = core::io::Latch<DataPort, MSBLatch>;
-using AddressLSB = core::io::Latch<DataPort, LSBLatch>;
-using AddressPort = core::io::WordExtend<AddressMSB, AddressLSB>;
+using MSBLatch = ActiveHigh<PortC::Bit<5>>;
+using LSBLatch = ActiveHigh<PortC::Bit<4>>;
+using AddressMSB = Latch<DataPort, MSBLatch>;
+using AddressLSB = Latch<DataPort, LSBLatch>;
+using AddressPort = WordExtend<AddressMSB, AddressLSB>;
 
 // Bus control lines
-using ReadEnable = core::io::ActiveLow<PortC::Bit<3>>;
-using WriteEnable = core::io::ActiveLow<PortC::Bit<2>>;
+using ReadEnable = ActiveLow<PortC::Bit<3>>;
+using WriteEnable = ActiveLow<PortC::Bit<2>>;
 using Control = core::io::Control<ReadEnable, WriteEnable>;
 
 using Bus = core::io::Bus<AddressPort, DelayRead<DataPort>, Control>;
@@ -81,21 +88,21 @@ void loop() {
 
 void set_lsb(Args args) {
   uint8_t data = 0;
-  core::mon::parse_unsigned(data, args.next());
+  parse_unsigned(data, args.next());
   AddressLSB::config_output();
   AddressLSB::write(data);
 }
 
 void set_msb(Args args) {
   uint8_t data = 0;
-  core::mon::parse_unsigned(data, args.next());
+  parse_unsigned(data, args.next());
   AddressMSB::config_output();
   AddressMSB::write(data);
 }
 
 void set_data(Args args) {
   uint8_t data = 0;
-  core::mon::parse_unsigned(data, args.next());
+  parse_unsigned(data, args.next());
   DataPort::config_output();
   DataPort::write(data);
 }
@@ -103,15 +110,15 @@ void set_data(Args args) {
 void write_bus(Args args) {
   uint16_t addr = 0;
   uint8_t data = 0;
-  core::mon::parse_unsigned(addr, args.next());
-  core::mon::parse_unsigned(data, args.next());
+  parse_unsigned(addr, args.next());
+  parse_unsigned(data, args.next());
   Bus::config_write();
   Bus::write_byte(addr, data);
 }
 
 void read_bus(Args args) {
   uint16_t addr = 0;
-  core::mon::parse_unsigned(addr, args.next());
+  parse_unsigned(addr, args.next());
   Bus::config_read();
   uint8_t data = Bus::read_byte(addr);
   serialEx.println(data, HEX);
