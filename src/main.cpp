bdc305cdae3d894040e5b235c02a2708f125e2eb
Trevor Makes <the.trevor.makes@gmail.com>
2022-12-07 21:57:23 -0800

    Move unused commands to separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) 2022 Trevor Makes
 
+#include "paged_write.hpp"
+
 #include "core/io.hpp"
 #include "core/io/bus.hpp"
 #include "core/cli.hpp"
@@ -7,8 +9,6 @@
 
 #include <Arduino.h>
 
-using core::cli::Args;
-using core::mon::parse_unsigned;
 using core::serial::StreamEx;
 using CLI = core::cli::CLI<>;
 
@@ -36,6 +36,7 @@ struct DelayRead : DATA {
   }
 };
 
+// Arduino Nano and Uno
 #ifdef __AVR_ATmega328P__
 
 // Create Port* wrappers around AVR ports B, C, D
@@ -43,10 +44,23 @@ CORE_PORT(B)
 CORE_PORT(C)
 CORE_PORT(D)
 
+//      * - D2 |         | x
+//      * - D3 |         | x
+// Data 4 - D4 |         | C5 - MSB Latch Enable
+// Data 5 - D5 |         | C4 - LSB Latch Enable
+// Data 6 - D6 | Arduino | C3 - Bus Read Enable
+// Data 7 - D7 |   NANO  | C2 - Bus Write Enable
+// Data 0 - B0 |         | C1 - *
+// Data 1 - B1 |         | C0 - *
+// Data 2 - B2 |   ___   | x
+// Data 3 - B3 |  |USB|  | x
+//      * - B4 |__|___|__| B5 - *
+// * unused
+
 // 8-bit data bus [D7 D6 D5 D4 B3 B2 B1 B0]
 using DataPort = BitExtend<PortD::Mask<0xF0>, PortB::Mask<0x0F>>;
 
-// Latch upper and lower bytes of address from data port
+// Latch upper and lower bytes of 16-bit address from data port
 using MSBLatch = ActiveHigh<PortC::Bit<5>>;
 using LSBLatch = ActiveHigh<PortC::Bit<4>>;
 using AddressMSB = Latch<DataPort, MSBLatch>;
@@ -72,104 +86,6 @@ void setup() {
   while (!Serial) {}
 }
 
-void set_lsb(Args);
-void set_msb(Args);
-void set_data(Args);
-void write_bus(Args);
-void read_bus(Args);
-
-void measure_page_write(Args);
-
-template <typename BUS, uint8_t PAGE_SIZE = 64>
-struct PagedWrite : BUS {
-  using typename BUS::DATA_TYPE;
-  using typename BUS::ADDRESS_TYPE;
-
-  static_assert(core::util::is_power_of_two(PAGE_SIZE), "");
-  static constexpr const ADDRESS_TYPE INDEX_MASK = PAGE_SIZE - 1;
-  static constexpr const ADDRESS_TYPE PAGE_MASK = ~INDEX_MASK;
-
-  static DATA_TYPE page_data[PAGE_SIZE];
-  static bool page_mask[PAGE_SIZE]; // TODO compact bitfield instead of bool array?
-  static ADDRESS_TYPE cached_page;
-
-  static void write_data(ADDRESS_TYPE address, DATA_TYPE data) {
-    // If last address was on a different page, flush the last page
-    ADDRESS_TYPE page = address & PAGE_MASK;
-    if (page != cached_page) {
-      flush_write();
-      cached_page = page;
-    }
-    // Cache data until page is flushed
-    uint8_t index = address & INDEX_MASK;
-    page_data[index] = data;
-    page_mask[index] = true;
-  }
-
-  static ADDRESS_TYPE poll_address;
-  static DATA_TYPE poll_data;
-  static bool is_flushing;
-
-  static void flush_write() {
-    // Poll until last BUS write is complete (max 10ms)
-    if (is_flushing) {
-      BUS::config_read();
-      while (BUS::read_data(poll_address) != poll_data) {}
-      is_flushing = false;
-    }
-    // Flush cached page data to BUS
-    BUS::config_write();
-    for (uint8_t i = 0; i < PAGE_SIZE; ++i) { 
-      if (page_mask[i]) {
-        ADDRESS_TYPE address = cached_page + i;
-        DATA_TYPE data = page_data[i];
-        BUS::write_data(address, data);
-        page_mask[i] = false;
-        // Save last written data so next flush can poll until EEPROM is ready
-        poll_address = address;
-        poll_data = data;
-        is_flushing = true;
-      }
-    }
-    // Propagate flush to parent type in case it does something
-    BUS::flush_write();
-  }
-};
-
-template <typename BUS, uint8_t PAGE_SIZE>
-typename BUS::DATA_TYPE PagedWrite<BUS, PAGE_SIZE>::page_data[PAGE_SIZE];
-
-template <typename BUS, uint8_t PAGE_SIZE>
-bool PagedWrite<BUS, PAGE_SIZE>::page_mask[PAGE_SIZE] = {}; // zero-initialized
-
-template <typename BUS, uint8_t PAGE_SIZE>
-typename BUS::ADDRESS_TYPE PagedWrite<BUS, PAGE_SIZE>::cached_page;
-
-template <typename BUS, uint8_t PAGE_SIZE>
-typename BUS::ADDRESS_TYPE PagedWrite<BUS, PAGE_SIZE>::poll_address;
-
-template <typename BUS, uint8_t PAGE_SIZE>
-typename BUS::DATA_TYPE PagedWrite<BUS, PAGE_SIZE>::poll_data;
-
-template <typename BUS, uint8_t PAGE_SIZE>
-bool PagedWrite<BUS, PAGE_SIZE>::is_flushing = false;
-
-// Hook write_data to measure IHX transfer rate
-template <typename BUS>
-struct MeasureImport : BUS {
-  using typename BUS::DATA_TYPE;
-  using typename BUS::ADDRESS_TYPE;
-
-  static void write_data(ADDRESS_TYPE address, DATA_TYPE data) {
-    static auto t1 = millis();
-    if ((address & 63) == 63) {
-      auto t2 = millis();
-      serialEx.println(t2 - t1);
-      t1 = t2;
-    }
-  }
-};
-
 // Define interface for core::mon function templates
 struct API : core::mon::Base<API> {
   using BUS = PagedWrite<Bus>;
@@ -179,11 +95,6 @@ struct API : core::mon::Base<API> {
 
 void loop() {
   static const core::cli::Command commands[] = {
-    { "lsb", set_lsb },
-    { "msb", set_msb },
-    { "data", set_data },
-    { "write", write_bus },
-    { "read", read_bus },
     { "hex", core::mon::cmd_hex<API> },
     { "set", core::mon::cmd_set<API> },
     { "fill", core::mon::cmd_fill<API> },
@@ -191,73 +102,7 @@ void loop() {
     { "export", core::mon::cmd_export<API> },
     { "import", core::mon::cmd_import<API> },
     { "verify", core::mon::cmd_verify<API> },
-    { "measure", measure_page_write },
   };
 
   serialCli.run_once(commands);
-}
-
-void set_lsb(Args args) {
-  uint8_t data = 0;
-  parse_unsigned(data, args.next());
-  AddressLSB::config_output();
-  AddressLSB::write(data);
-}
-
-void set_msb(Args args) {
-  uint8_t data = 0;
-  parse_unsigned(data, args.next());
-  AddressMSB::config_output();
-  AddressMSB::write(data);
-}
-
-void set_data(Args args) {
-  uint8_t data = 0;
-  parse_unsigned(data, args.next());
-  DataPort::config_output();
-  DataPort::write(data);
-}
-
-void write_bus(Args args) {
-  uint16_t addr = 0;
-  uint8_t data = 0;
-  parse_unsigned(addr, args.next());
-  parse_unsigned(data, args.next());
-  Bus::config_write();
-  Bus::write_data(addr, data);
-
-  // Measure data polling to completion
-  Bus::config_read();
-  auto t1 = millis();
-  while (Bus::read_data(addr) != data) {}
-  auto t2 = millis();
-  serialEx.println(t2 - t1);
-}
-
-void read_bus(Args args) {
-  uint16_t addr = 0;
-  parse_unsigned(addr, args.next());
-  Bus::config_read();
-  uint8_t data = Bus::read_data(addr);
-  serialEx.println(data, HEX);
-}
-
-// Measure EEPROM page load/write timing
-void measure_page_write(Args) {
-  Bus::config_write();
-
-  // Measure 64 byte page load
-  auto t1 = micros();
-  for (uint16_t addr = 0x140; addr < 0x180; ++addr) {
-    Bus::write_data(addr, addr);
-  }
-  auto t2 = micros();
-
-  // Measure data polling to completion
-  Bus::config_read();
-  while (Bus::read_data(0x17F) != 0x7F) {}
-  auto t3 = micros();
-
-  serialEx.println(t2 - t1);
-  serialEx.println(t3 - t2);
 }
---
+++ src/paged_write.hpp
@@ -0,0 +1,81 @@
+// Copyright (c) 2022 Trevor Makes
+
+#pragma once
+
+#include "core/util.hpp"
+
+#include <stdint.h>
+
+template <typename BUS, uint8_t PAGE_SIZE = 64>
+struct PagedWrite : BUS {
+  using typename BUS::DATA_TYPE;
+  using typename BUS::ADDRESS_TYPE;
+
+  static_assert(core::util::is_power_of_two(PAGE_SIZE), "");
+  static constexpr const ADDRESS_TYPE INDEX_MASK = PAGE_SIZE - 1;
+  static constexpr const ADDRESS_TYPE PAGE_MASK = ~INDEX_MASK;
+
+  static DATA_TYPE page_data[PAGE_SIZE];
+  static bool page_mask[PAGE_SIZE]; // TODO compact bitfield instead of bool array?
+  static ADDRESS_TYPE cached_page;
+
+  static void write_data(ADDRESS_TYPE address, DATA_TYPE data) {
+    // If last address was on a different page, flush the last page
+    ADDRESS_TYPE page = address & PAGE_MASK;
+    if (page != cached_page) {
+      flush_write();
+      cached_page = page;
+    }
+    // Cache data until page is flushed
+    uint8_t index = address & INDEX_MASK;
+    page_data[index] = data;
+    page_mask[index] = true;
+  }
+
+  static ADDRESS_TYPE poll_address;
+  static DATA_TYPE poll_data;
+  static bool is_flushing;
+
+  static void flush_write() {
+    // Poll until last BUS write is complete (max 10ms)
+    if (is_flushing) {
+      BUS::config_read();
+      while (BUS::read_data(poll_address) != poll_data) {}
+      is_flushing = false;
+    }
+    // Flush cached page data to BUS
+    BUS::config_write();
+    for (uint8_t i = 0; i < PAGE_SIZE; ++i) { 
+      if (page_mask[i]) {
+        ADDRESS_TYPE address = cached_page + i;
+        DATA_TYPE data = page_data[i];
+        BUS::write_data(address, data);
+        page_mask[i] = false;
+        // Save last written data so next flush can poll until EEPROM is ready
+        poll_address = address;
+        poll_data = data;
+        is_flushing = true;
+      }
+    }
+    // Propagate flush to parent type in case it does something
+    BUS::flush_write();
+  }
+};
+
+template <typename BUS, uint8_t PAGE_SIZE>
+typename BUS::DATA_TYPE PagedWrite<BUS, PAGE_SIZE>::page_data[PAGE_SIZE];
+
+template <typename BUS, uint8_t PAGE_SIZE>
+bool PagedWrite<BUS, PAGE_SIZE>::page_mask[PAGE_SIZE] = {}; // zero-initialized
+
+template <typename BUS, uint8_t PAGE_SIZE>
+typename BUS::ADDRESS_TYPE PagedWrite<BUS, PAGE_SIZE>::cached_page;
+
+template <typename BUS, uint8_t PAGE_SIZE>
+typename BUS::ADDRESS_TYPE PagedWrite<BUS, PAGE_SIZE>::poll_address;
+
+template <typename BUS, uint8_t PAGE_SIZE>
+typename BUS::DATA_TYPE PagedWrite<BUS, PAGE_SIZE>::poll_data;
+
+template <typename BUS, uint8_t PAGE_SIZE>
+bool PagedWrite<BUS, PAGE_SIZE>::is_flushing = false;
