3ecc8d60a8171c6ceb2670c5474bd69c56619bb1
Trevor Makes <the.trevor.makes@gmail.com>
2022-12-09 01:36:01 -0800

    Rename start_address to base_address

--- scripts/bin2ihx.py
+++ scripts/bin2ihx.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('infile')
 parser.add_argument('outfile')
 parser.add_argument('-r', '--record_size', type=int, default=32, help='bytes of binary data per line of text')
-parser.add_argument('-s', '--start_address', type=int, default=0, help='where the binary data will be loaded into memory')
+parser.add_argument('-b', '--base_address', type=int, default=0, help='where the binary data will be loaded into memory')
 args = parser.parse_args()
 
 REC_SIZE = args.record_size
@@ -19,7 +19,7 @@ with open(args.outfile, mode="w") as f:
     size = len(rec)
 
     # TODO add support for 24/32 bit addresses using 02/04 record types
-    address = index + args.start_address
+    address = index + args.base_address
     f.write(f':{size:02X}{address:04X}00')
     checksum = size + (address >> 8) + (address & 0xFF)
     index += REC_SIZE
