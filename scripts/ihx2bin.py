7b22548a8a4192e133596f24ccc03b0cff9a21a8
Trevor Makes <the.trevor.makes@gmail.com>
2022-12-09 01:32:51 -0800

    Add Intel IHX to binary conversion script

---
+++ scripts/ihx2bin.py
@@ -0,0 +1,41 @@
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infile')
+parser.add_argument('outfile')
+args = parser.parse_args()
+
+sparse = {}
+
+with open(args.infile, mode="r") as f:
+  for line in f:
+    try:
+      line = line.split(':', 1)[1]
+    except:
+      continue
+
+    size = int(line[0:2], base=16)
+    address = int(line[2:6], base=16)
+    type = int(line[6:8], base=16)
+    checksum = size + (address >> 8) + (address & 0xFF) + type
+
+    end = 8 + 2 * size
+    for i, v in enumerate(re.findall('..', line[8:end])):
+      data = int(v, base=16)
+      sparse[address + i] = data
+      checksum += data
+
+    neg_checksum = int(line[end:end+2], base=16)
+    assert((checksum + neg_checksum) & 0xFF == 0)
+
+base = min(sparse.keys())
+size = 1 + max(sparse.keys()) - base
+
+dense = bytearray(size)
+for i, v in sparse.items():
+  dense[i - base] = v
+
+print('Base address:', base)
+with open(args.outfile, mode='wb') as f:
+  f.write(dense)
