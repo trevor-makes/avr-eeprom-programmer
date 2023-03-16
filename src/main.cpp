d466942d185d94f4baf775143b0eaf545e325805
Trevor Makes <the.trevor.makes@gmail.com>
2023-03-16 02:34:58 -0700

    Working changes for 28C burner + 23 reader

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ using core::io::ActiveLow;
 using core::io::ActiveHigh;
 using core::io::BitExtend;
 using core::io::WordExtend;
+using core::io::LeftShift;
+using core::io::Overlay;
 using core::io::Latch;
 
 // Arduino Nano and Uno
@@ -24,32 +26,53 @@ CORE_PORT(B)
 CORE_PORT(C)
 CORE_PORT(D)
 
-//      * - D2 |         | x
-//      * - D3 |         | x
-// Data 4 - D4 |         | C5 - MSB Latch Enable (active high)
-// Data 5 - D5 |         | C4 - LSB Latch Enable (active high)
-// Data 6 - D6 | Arduino | C3 - Bus Read Enable (active low)
-// Data 7 - D7 |   NANO  | C2 - Bus Write Enable (active low)
-// Data 0 - B0 |         | C1 - *
-// Data 1 - B1 |         | C0 - *
-// Data 2 - B2 |   ___   | x
-// Data 3 - B3 |  |USB|  | x
-//      * - B4 |__|___|__| B5 - *
+// LSB Latch - D2 |         | x
+// MSB Latch - D3 |         | x
+//    Data 4 - D4 |         | C5 - Write Enable (active low)
+//    Data 5 - D5 |         | C4 - Mode Select (input)
+//    Data 6 - D6 | Arduino | C3 - Read Enable (active low)
+//    Data 7 - D7 |   NANO  | C2 - Chip Enable (active low)
+//    Data 0 - B0 |         | C1 - *
+//    Data 1 - B1 |         | C0 - *
+//    Data 2 - B2 |   ___   | x
+//    Data 3 - B3 |  |USB|  | x
+//         * - B4 |__|___|__| B5 - *
 // * unused digital pins
 
 // 8-bit data bus [D7 D6 D5 D4 B3 B2 B1 B0]
 using DataPort = BitExtend<PortD::Mask<0xF0>, PortB::Mask<0x0F>>;
 
 // Latch upper and lower bytes of 16-bit address from data port
-using MSBLatch = ActiveHigh<PortC::Bit<5>>;
-using LSBLatch = ActiveHigh<PortC::Bit<4>>;
+using MSBLatch = ActiveHigh<PortD::Bit<3>>;
+using LSBLatch = ActiveHigh<PortD::Bit<2>>;
 using AddressMSB = Latch<DataPort, MSBLatch>;
 using AddressLSB = Latch<DataPort, LSBLatch>;
 using AddressPort = WordExtend<AddressMSB, AddressLSB>;
 
+
+// TODO make Overlay work with ROMLatch and A11; use instead of AddressMSB_ROM
+struct AddressMSB_ROM {
+  using ROMData = Overlay<LeftShift<PortB::Mask<0x08>, 1>, PortB::Mask<0x07>>;
+  using ROMLatch = Latch<ROMData, MSBLatch>;
+  using A11 = LeftShift<PortC::Bit<2>, 1>;
+
+  using TYPE = uint8_t;
+  static inline void config_output() {
+    ROMLatch::config_output();
+    A11::config_output();
+  }
+  static inline void write(TYPE data) {
+    ROMLatch::write(data);
+    A11::write(data);
+  }
+};
+using AddressPort_ROM = WordExtend<AddressMSB_ROM, AddressLSB>;
+
 // Bus control lines
+using ChipEnable = ActiveLow<PortC::Bit<2>>;
 using ReadEnable = ActiveLow<PortC::Bit<3>>;
-using WriteEnable = ActiveLow<PortC::Bit<2>>;
+using ModeSelect = PortC::Bit<4>;
+using WriteEnable = ActiveLow<PortC::Bit<5>>;
 
 #else
 #error Need to provide configuration for current platform. See __AVR_ATmega328P__ configuration above.
@@ -57,6 +80,7 @@ using WriteEnable = ActiveLow<PortC::Bit<2>>;
 
 // See read and write timings in bus.hpp
 using Bus = PortBus<AddressPort, DataPort, ReadEnable, WriteEnable>;
+using Bus_ROM = PortBus<AddressPort_ROM, DataPort, ReadEnable, WriteEnable>;
 
 using core::serial::StreamEx;
 using CLI = core::cli::CLI<>;
@@ -74,11 +98,12 @@ struct API : core::mon::Base<API> {
   using BUS = PagedWrite<Bus>;
 };
 
-void setup() {
-  // Establish serial connection with computer
-  Serial.begin(9600);
-  while (!Serial) {}
-}
+struct API_ROM : core::mon::Base<API_ROM> {
+  static StreamEx& get_stream() { return serialEx; }
+  static CLI& get_cli() { return serialCli; }
+  // Wrap bus with paged write adapter to meet EEPROM write timings
+  using BUS = Bus_ROM;
+};
 
 void set_baud(Args args) {
   CORE_EXPECT_UINT(API, uint32_t, baud, args, return)
@@ -125,7 +150,15 @@ void lock(Args) {
   delay(10); // lock takes up to 10 ms
 }
 
-void loop() {
+template <typename BUS>
+void debug_bus(Args args) {
+  CORE_EXPECT_UINT(API, uint16_t, addr, args, return)
+  CORE_EXPECT_UINT(API, uint8_t, data, args, return)
+  BUS::config_write();
+  BUS::write_bus(addr, data);
+}
+
+void loop_eeprom() {
   static const core::cli::Command commands[] = {
     { "baud", set_baud },
     { "hex", core::mon::cmd_hex<API> },
@@ -138,12 +171,48 @@ void loop() {
     { "erase", erase },
     { "unlock", unlock },
     { "lock", lock },
-    // Debug commands for measuring read/write throughput
-    //{ "write", write_bus<API> },
-    //{ "read", read_bus<API> },
-    //{ "pagew", page_write<API> },
-    //{ "pager", page_read<API> },
+    { "debug", debug_bus<Bus> },
   };
 
   serialCli.run_once(commands);
+}
+
+void loop_rom() {
+  static const core::cli::Command commands[] = {
+    { "baud", set_baud },
+    { "hex", core::mon::cmd_hex<API_ROM> },
+    { "export", core::mon::cmd_export<API_ROM> },
+    { "verify", core::mon::cmd_verify<API_ROM> },
+    { "debug", debug_bus<Bus_ROM> },
+  };
+
+  serialCli.run_once(commands);
+}
+
+void (*loop_fn)();
+
+void setup() {
+  // Establish serial connection with computer
+  Serial.begin(9600);
+  while (!Serial) {}
+
+  ModeSelect::config_input();
+  AddressMSB::config_output();
+  AddressMSB::write(0); // set A13 low
+
+  // Read if jumper set to Vcc or A13 (low)
+  if (ModeSelect::is_set()) {
+    Serial.println("2364 ROM mode");
+    loop_fn = loop_rom;
+  } else {
+    // TODO set A13 high and check ModeSelect now set
+    Serial.println("28C EEPROM mode");
+    ChipEnable::config_output();
+    ChipEnable::enable();
+    loop_fn = loop_eeprom;
+  }
+}
+
+void loop() {
+  loop_fn();
 }
